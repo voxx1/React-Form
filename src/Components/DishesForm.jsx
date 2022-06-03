ffcea370a180d21461b65b55aabd9096d5784265
voxx1 <61800692+voxx1@users.noreply.github.com>
2022-06-03 17:35:39 +0200

    final version of form

--- src/Components/DishesForm.jsx
+++ src/Components/DishesForm.jsx
@@ -1,34 +1,35 @@
 import { useState } from 'react';
 import { Form, Field } from 'react-final-form'
 import classes from './Form.module.css';
+import LoadingSpinner from './LoadingSpinner';
 
 const DishesForm = () => {
 
     const [dishDeatils, setDishDetails] = useState([])
     const [selectedType, setSelectedType] = useState()
     const [isLoading, setIsLoading] = useState(false);
-
     const radios = ["pizza", "soup", "sandwich"]
+
     const handleClick = radio => event => setSelectedType(radio);
 
     const submitHandler = (event) => {
-        event.preventDefault();
-
         setIsLoading(true);
-        fetch("", {
+        fetch("https://frosty-wood-6558.getsandbox.com:443/dishes", {
             method: 'POST',
             body: JSON.stringify({
                 name: dishDeatils.dishName,
-                preparationTime: dishDeatils.preparationTime,
+                preparation_time: dishDeatils.preparationTime,
                 type: dishDeatils.type,
-
+                spiciness_scale: Number(dishDeatils.spicenessNumber),
+                slices_of_bread: Number(dishDeatils.breadSlicesNumber),
+                no_of_slices: Number(dishDeatils.pizzaSlicesNumber),
+                diameter: Number(dishDeatils.diameterNumber)
             }),
             headers: {
                 'Content-Type': 'application/json',
             },
         })
             .then((res) => {
-                setIsLoading(false);
                 if (res.ok) {
                     return res.json();
                 } else {
@@ -41,11 +42,12 @@ const DishesForm = () => {
             .catch((err) => {
                 alert(err.message);
             });
+        setIsLoading(false)
     };
 
     let choosenType = ""
 
-    if (selectedType == "pizza") {
+    if (selectedType === "pizza") {
         choosenType =
             <>
                 <div className={classes.control}>
@@ -54,17 +56,17 @@ const DishesForm = () => {
                 </div>
                 <div className={classes.control}>
                     <label>Diameter: </label>
-                    <Field name='diameterNumber' type='number' id="diameterNumber" min="0" component="input" required />
+                    <Field name='diameterNumber' type='float' id="diameterNumber" min="0" component="input" required />
                 </div>
             </>
 
-    } else if (selectedType == "soup") {
+    } else if (selectedType === "soup") {
         choosenType =
             <div className={classes.control}>
                 <label>Spiceness scale: </label>
                 <Field name='spicenessNumber' type='number' id="spicenessNumber" min="0" max="10" component="input" required />
             </div>
-    } else if (selectedType == "sandwich") {
+    } else if (selectedType === "sandwich") {
         choosenType =
             <div className={classes.control}>
                 <label>Slices of bread: </label>
@@ -75,55 +77,56 @@ const DishesForm = () => {
 
     return (
         <div className={classes.background} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <section className={classes.auth}>
-                <Form
-                    onSubmit={submitHandler}
-                    render={({ handleSubmit, values, form: { getState } }) => (
-                        <form onSubmit={handleSubmit}>
-                            <h1>Dishes App</h1>
-                            <div className={classes.control}>
-                                <label htmlFor='dishName'>Dish name</label>
-                                <Field name='dishName' type='text' id="dishName" component="input" required />
-                            </div>
-                            <div className={classes.control}>
-                                <label htmlFor='preparationTime'>Preparation time</label>
-                                <Field step="2" name='preparationTime' type='time' id="preparationTime" component="input" required />
-                            </div>
-                            <div className={classes.control}>
-                                <div className={classes.radio}>
-                                    <label style={{ marginRight: "15px" }} htmlFor='dishType'>Type of dish</label>
-                                    {radios.map(radioType => {
-                                        return (
-                                            <label key={radioType}>
-                                                <Field
-                                                    name="type"
-                                                    component="input"
-                                                    type="radio"
-                                                    value={radioType}
-                                                    onClick={handleClick(radioType)}
-                                                />{' '}
-                                                {radioType}
-                                            </label>
-                                        )
-                                    })}
-                                </div>
-                            </div>
-                            {choosenType}
-                            <div className={classes.actions}>
-                                {!isLoading && (
-                                    <button onClick={() => setDishDetails(getState().values)} type='button'
-                                        className={classes.toggle}>Submit</button>
-                                )}
-                                {isLoading && <p>Sending request...</p>}
-                            </div>
-                            <pre>{JSON.stringify(values, 0, 2)}</pre>
-
-                        </form>
-                    )}
-                />
+            <section className={classes.formBody}>
+                {isLoading ? <LoadingSpinner /> :
+                    <>
+                        <Form
+                            onSubmit={submitHandler}
+                            render={({ handleSubmit, values, form: { getState } }) => (
+                                <form onSubmit={handleSubmit}>
+                                    <h1>Dishes App</h1>
+                                    <div className={classes.control}>
+                                        <label htmlFor='dishName'>Dish name</label>
+                                        <Field name='dishName' type='text' id="dishName" component="input" required />
+                                    </div>
+                                    <div className={classes.control}>
+                                        <label htmlFor='preparationTime'>Preparation time</label>
+                                        <Field step="2" name='preparationTime' type='time' id="preparationTime" component="input" required />
+                                    </div>
+                                    <div className={classes.control}>
+                                        <div className={classes.radio}>
+                                            <label style={{ marginRight: "15px" }} htmlFor='dishType'>Type of dish</label>
+                                            {radios.map(radioType => {
+                                                return (
+                                                    <label key={radioType}>
+                                                        <Field
+                                                            name="type"
+                                                            component="input"
+                                                            type="radio"
+                                                            value={radioType}
+                                                            onClick={handleClick(radioType)}
+                                                        />{' '}
+                                                        {radioType}
+                                                    </label>
+                                                )
+                                            })}
+                                        </div>
+                                    </div>
+                                    {choosenType}
+                                    <div className={classes.actions}>
+                                        {!isLoading && (
+                                            <button onClick={() => setDishDetails(getState().values)} type='submit'
+                                                className={classes.toggle}>Submit</button>
+                                        )}
+                                        {isLoading && <p style={{ fontWeight: "bold", marginTop: "-10px" }}>Form submitted!</p>}
+                                    </div>
+                                </form>
+                            )}
+                        />
+                    </>
+                }
             </section>
         </div>
-
     );
 };
 
